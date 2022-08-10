1c0e478e82a154756905affeef7a17fd8dadad94
chenrohua24 <z7707092003@gmail.com>
2022-08-10 23:25:28 +0800

    feat: custom handle ma's schema validation error

--- api/decorators.py
+++ api/decorators.py
@@ -1,6 +1,24 @@
 from functools import wraps
 
-from webargs.flaskparser import use_args
+from webargs.flaskparser import FlaskParser as BaseFlaskParser
+from api.errors import ValidationError
+
+
+class FlaskParser(BaseFlaskParser):
+    DEFAULT_VALIDATION_STATUS = 400
+
+    def handle_error(
+        self, error, req, schema,
+        *, error_status_code, error_headers,
+    ):
+        raise ValidationError(
+            error_status_code or self.DEFAULT_VALIDATION_STATUS,
+            error.messages,
+        )
+
+
+parser = FlaskParser()
+use_args = parser.use_args
 
 
 def response(schema, status_code=200):
--- api/errors.py
+++ api/errors.py
@@ -3,6 +3,22 @@ from flask import Blueprint
 blueprint = Blueprint("errors", __name__)
 
 
+class ValidationError(Exception):
+    def __init__(self, status_code, messages):
+        self.status_code = status_code
+        self.messages = messages
+
+
+@blueprint.app_errorhandler(ValidationError)
+def validation_error(e):
+    return {
+        "code": e.status_code,
+        "message": "Validation Error",
+        "description": "Some errors found below when parsing",
+        "errors": e.messages
+    }, e.status_code
+
+
 @blueprint.app_errorhandler(Exception)
 def unexpected(e):
     return {

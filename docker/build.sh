d5e9e28c4b6dbfaf4f03d5fa70844a12dc0231de
chenrohua24 <z7707092003@gmail.com>
2022-08-18 22:16:14 +0800

    refactor: fix static variable

--- docker/build.sh
+++ docker/build.sh
@@ -5,7 +5,6 @@ set -xe
 set -o pipefail
 
 # declare
-FLASK_CONFIG=$1
 DOCKERFILE_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ -z "$FLASK_CONFIG" ]; then
@@ -15,7 +14,6 @@ fi
 
 # build docker image
 docker build \
---build-arg FLASK_CONFIG=$FLASK_CONFIG \
 -t presale:latest \
 -f $DOCKERFILE_DIR_PATH/Dockerfile .
 

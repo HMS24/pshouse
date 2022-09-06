cb37d986e866c4aaf00e014ea9f2e8b0b935d47a
chenrohua24 <z7707092003@gmail.com>
2022-09-06 23:17:01 +0800

    chore: rename env variable

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL") or \
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI") or \
         "sqlite:///" + os.path.join(basedir, "app.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
@@ -26,7 +26,7 @@ class TestConfig(Config):
     DEBUG = False
     SQLALCHEMY_RECORD_QUERIES = False
     SQLALCHEMY_ECHO = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URL") or "sqlite:///:memory:"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("TEST_DATABASE_URI") or "sqlite:///:memory:"
 
 
 config = {

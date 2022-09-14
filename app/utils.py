24c8849fbaa6cf55abc7e71745b3413d2406b09e
chenrohua24 <z7707092003@gmail.com>
2022-09-14 22:16:46 +0800

    fix: type bug

--- app/utils.py
+++ app/utils.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 from app import config
 
 
-def generate_start_end_timestamp(date, days=config.DATA_REVEAL_DAYS):
+def generate_start_end_timestamp(date, days=int(config.DATA_REVEAL_DAYS)):
     start = datetime.timestamp(date - timedelta(days=days))
     end = datetime.timestamp(date)
 
